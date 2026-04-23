8ab23b45555ba79bf5101d564e6122e448f0353b
walker <walker.wzy@gmail.com>
2026-04-24 03:57:47 +0800

    fix: add __init__.py files and ensure handler export

--- api/index.py
+++ api/index.py
@@ -1,9 +1,4 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from mangum import Mangum
-from refactored_router.main import app as fastapi_app
+from ..refactored_router.main import app as fastapi_app
 
 handler = Mangum(fastapi_app)
