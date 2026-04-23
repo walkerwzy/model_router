aafc5d5db7ed3b160305602ee45f7bf7a5008279
walker <walker.wzy@gmail.com>
2026-04-24 02:26:19 +0800

    fix: vercel path setup

--- api/index.py
+++ api/index.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from mangum import Mangum
 from refactored_router.main import app as fastapi_app
 
